8542832ed281f682da9bed8d5d090929fd42bace
xcmdx <potekhen.pasha91@gmail.com>
2021-10-13 22:50:25 +0700

    add files

--- test.py
+++ test.py
@@ -1,10 +1,23 @@
+def Vudal(N, K):  # таблица Вудала
+    # N- кол-во строк, K - кол-во столбцов
+    mas = [[0 for _ in range(K)] for _ in range(N)]
+    for n in range(N):
+        for k in range(K):
+            if (n + 2 > k):
+                mas[n][k] = (n + 1) * (k + 1) ** (n + 1) - 1
+            else:
+                mas[n][k] = 0
+    return mas, N, K
+
+
 def check(string):
     try:
         string = int(string)
     except ValueError:
         print('\n',
-            'aazdsad',
+            "Error_1",
             '\n')
     return string
 
-print(type(check(input())))
+
+while True:
